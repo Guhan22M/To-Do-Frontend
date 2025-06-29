b222dfba28e1acdc21bfb3d78a6374faccdbc95f
Guhan22M <guhan2218@gmail.com>
2025-06-29 20:31:54 +0530

    Initial commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -33,7 +33,7 @@ function Navbar() {
                   <Link className="nav-link" to="/login">Login</Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/register">Register</Link>
+                  <Link className="nav-link" to="/">Register</Link>
                 </li>
               </>
             ) : (
